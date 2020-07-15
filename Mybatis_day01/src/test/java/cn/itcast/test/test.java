15ffab2d83c56d1cd48b0ec72279b55e214c77dc
chenpeng <994839392@qq.com>
2020-07-15 18:01:02 +0800

    Mybatis xml Demo

--- Mybatis_day01/src/test/java/cn/itcast/test/test.java
+++ Mybatis_day01/src/test/java/cn/itcast/test/test.java
@@ -1,7 +1,34 @@
 package cn.itcast.test;
 
+import cn.itcast.Dao.UserMapper;
+import cn.itcast.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //读取配置文件
+        InputStream in = Resources.getResourceAsStream("MybatisConfig.xml");
+
+        // 创建工厂类
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(in);
+        // 通过工厂类获取UserMapper的实例化对象
+        SqlSession sqlSession = factory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        List<User> users = mapper.findAll();
+
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+
     }
 }
